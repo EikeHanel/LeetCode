08dbc3563283796cadbf9e2daf45df548732da6d
Eike Hanel <144679101+EikeHanel@users.noreply.github.com>
2024-09-14 15:44:09 +0200

    Time: 75 ms (84.56%), Space: 16.8 MB (64.35%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -3,7 +3,8 @@ class Solution:
         data = nums1 + nums2
         data.sort()
         mid = len(data) // 2
-        if not len(data) % 2:
-            return (data[mid - 1] + data[mid]) / 2.0
-        return data[mid]
+        return (data[mid] + data[~mid]) / 2.0
+        # if not len(data) % 2:
+        #     return (data[mid - 1] + data[mid]) / 2.0
+        # return data[mid]
        
