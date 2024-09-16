57fe266dd623b7211c419be38a52f6ed6a417618
Eike Hanel <144679101+EikeHanel@users.noreply.github.com>
2024-09-16 19:50:02 +0200

    Time: 50 ms (35.45%), Space: 16.9 MB (22.84%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,13 +1,50 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        test = False 
-        for n in range(len(s)-1):
-            if len(s) != len(p):
-                break
-            elif s[n] == p[n] or p[n] == ".":
-                test  = True
-            elif n > 0 and p[n] == "*":
-                if s[n] == p[n-1]:
-                    test = True
-        return test
-                    
+        # Memoization cache to store results of subproblems
+        memo = {}
+
+        def dp(i, j):
+            # Check if we've already computed this subproblem
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            # If we've reached the end of the pattern
+            if j == len(p):
+                if i == len(s):
+                    memo[(i, j)] = True  # Store True for a complete match
+                else:
+                    memo[(i, j)] = False  # Store False if the string still has characters
+                return memo[(i, j)]
+
+            # Check if the current character matches
+            current_match = False
+            if i < len(s) and (p[j] == s[i] or p[j] == '.'):
+                current_match = True
+
+            # If the next character in the pattern is '*'
+            if j + 1 < len(p) and p[j + 1] == '*':
+                # Option 1: Skip the "x*" (i.e., match zero occurrences of the character)
+                match_zero = dp(i, j + 2)
+                
+                # Option 2: Use the '*' to match one or more occurrences of the current character
+                match_one_or_more = False
+                if current_match:
+                    match_one_or_more = dp(i + 1, j)
+                
+                # If either option works, return True
+                if match_zero or match_one_or_more:
+                    memo[(i, j)] = True  # Store True for a match
+                else:
+                    memo[(i, j)] = False  # Store False if neither option works
+                return memo[(i, j)]
+            
+            # If there's no '*', check if the first character matches and move forward
+            if current_match:
+                memo[(i, j)] = dp(i + 1, j + 1)  # Proceed to the next character in both s and p
+            else:
+                memo[(i, j)] = False  # If no match at the current position, return False
+
+            return memo[(i, j)]
+
+        # Start the recursion from the beginning of both the string and the pattern
+        return dp(0, 0)
