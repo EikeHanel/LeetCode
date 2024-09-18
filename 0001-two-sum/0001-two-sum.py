b13f8c78719c7dba42a4720e1f3b2d8d87c8ce91
Eike Hanel <144679101+EikeHanel@users.noreply.github.com>
2024-09-18 13:27:47 +0200

    Time: 58 ms (68.7%), Space: 17.7 MB (30.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ans = []
-        for num in range(len(nums)):
-            for n in range(len(nums)):
-                if n != num and num < n:
-                    test_target = nums[n] + nums[num]
-                    if test_target == target:
-                        ans.append(num)
-                        ans.append(n)
-        return ans        
+        # New: using a dictionary  
+        num_dict = {}
+        for i, num in enumerate(nums):
+            missing_num = target-num
+            if missing_num in num_dict:
+                return [num_dict[missing_num], i]
+            else:
+                num_dict[num] = i
+
+        # Old: brute force
+        # ans = []
+        # for num in range(len(nums)):
+        #     for n in range(len(nums)):
+        #         if n != num and num < n:
+        #             test_target = nums[n] + nums[num]
+        #             if test_target == target:
+        #                 ans.append(num)
+        #                 ans.append(n)
+        # return ans        
             
