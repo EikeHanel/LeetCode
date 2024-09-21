aae7d1f57fc61a2f3d9f6021fa15dd7af3377f0a
Eike Hanel <144679101+EikeHanel@users.noreply.github.com>
2024-09-21 17:01:35 +0200

    Time: 35 ms (72.93%), Space: 16.6 MB (22.38%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,8 +1,5 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        idx = []
-        count= 0
-        
         for n in reversed(range(len(nums))):
             if nums[n] == val:
                 nums.pop(n)
