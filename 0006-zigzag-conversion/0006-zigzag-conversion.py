7807bae368b6610d515b489ef66e42e2473c9ac8
Eike Hanel <144679101+EikeHanel@users.noreply.github.com>
2024-09-26 20:44:03 +0200

    Time: 40 ms (95.73%), Space: 16.6 MB (86.69%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -6,14 +6,14 @@ class Solution:
 
         # Create a list of empty strings for each row
         rows = [''] * numRows
-        print(rows)
+        # print(rows)
         cur_row = 0
         going_down = False
 
         # Set char to fitting row
         for char in s:
             rows[cur_row] += char
-            print(f"row: {cur_row} val: {rows}")
+            # print(f"row: {cur_row} val: {rows}")
             # Change direction if we reach the top or bottom row
             if cur_row == 0 or cur_row == numRows - 1:
                 going_down = not going_down
