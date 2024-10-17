29c2c9fc5ae9789ab55ab671b3a62dd0c846db87
Eike Hanel <144679101+EikeHanel@users.noreply.github.com>
2024-10-17 16:39:52 +0200

    Time: 0 ms (100%), Space: 16.6 MB (10.8%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        nums = [int(n) for n in str(num)]
+        ans = ""
+        l = 0
+        max_num = 0
+        idx = 0
+        while l < len(nums):
+            for r in range(l, len(nums)):
+                if nums[l] < nums[r]:
+                    if max_num <= nums[r]:
+                        idx = r
+                    max_num = max(max_num, nums[r])
+            if max_num > 0:
+                nums[idx] = nums[l]
+                nums[l] = max_num
+                for n in nums:
+                    ans += str(n)
+                return int(ans)
+            l += 1
+
+        return num      
