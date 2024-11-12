69ac25d501385e49f2cb9ae6d8afda24d6673eaf
Eike Hanel <144679101+EikeHanel@users.noreply.github.com>
2024-11-12 16:27:03 +0100

    Time: 178 ms (62.2%), Space: 65.8 MB (63.44%) - LeetHub

---
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+        items.sort()
+        queries = sorted([(q, i) for i, q in enumerate(queries)])
+        ans = [0] * len(queries)
+        max_bea = 0
+        j = 0
+        for q, i in queries:
+
+            while j < len(items) and items[j][0] <= q:
+                max_bea = max(max_bea, items[j][1])
+                j += 1
+            ans[i]  = max_bea
+
+        return ans
