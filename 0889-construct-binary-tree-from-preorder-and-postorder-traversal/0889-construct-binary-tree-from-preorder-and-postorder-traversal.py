0a5cb7ad899bd04e6de955441d7db6bfa284d098
Eike Hanel <144679101+EikeHanel@users.noreply.github.com>
2025-02-23 12:49:14 +0100

    Time: 3 ms (50%), Space: 17.7 MB (71.53%) - LeetHub

---
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        N = len(postorder)
+        post_val_to_idx = {n: i for i, n in enumerate(postorder)}
+
+        def build(i1, i2, j1, j2):
+            if i1 > i2 or j1 > j2:
+                return None
+            root = TreeNode(preorder[i1])
+
+            if i1 != i2:
+                left_value = preorder[i1 + 1]
+                mid = post_val_to_idx[left_value]
+                left_size = mid - j1 + 1
+
+                root.left = build(i1 + 1, i1 + left_size, j1, mid)
+
+                root.right = build(i1 + left_size + 1, i2, mid + 1, j2 - 1)
+            return root
+
+
+        return build(0, N - 1, 0, N - 1)
